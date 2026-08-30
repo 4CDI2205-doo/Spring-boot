5e6feec364d5b23e86804ca18fa465e216e2628d
4CDI2205-doo <btja9985@yahoo.co.jp>
2026-08-30 21:59:12 +0900

    プロジェクト開始

---
+++ demo2/src/main/java/com/example2/demo2/Controller.java
@@ -0,0 +1,14 @@
+package com.example2.demo2;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public String Hello() {
+        return "Hello Spring Boot!";
+    }
+
+}
---
+++ front/js/header.js
@@ -0,0 +1,15 @@
+const head_script = `
+<header>
+    <div class="logo">
+        <a href="Home.html">仮ロゴ</a>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="register.html">登録</a></li>
+            <li><a href="list.html">表示</a></li>
+            <li><a href="edit.html">編集</a></li>
+        </ul>
+    </nav>
+    `;
+
+document.getElementById("headerplaceholder").innerHTML = head_script;
