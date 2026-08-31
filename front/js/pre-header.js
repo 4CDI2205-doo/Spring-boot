bdc1bf77c1a4d88ad824b1009f755fc1eb21f801
4CDI2205-doo <btja9985@yahoo.co.jp>
2026-08-31 23:02:05 +0900

    新規登録、ログインなどのフォームとデータ保管場所の作成他

---
+++ front/js/pre-header.js
@@ -0,0 +1,8 @@
+const head_script = `
+<header>
+    <div class="logo">
+        <a href="Home.html">仮ロゴ</a>
+    </div>
+    `;
+
+document.getElementById("pre-headerplaceholder").innerHTML = head_script;
---
+++ front/js/register.js
@@ -0,0 +1,13 @@
+document.getElementById("newlogin_form").addEventListener("submit",() => {
+    event.preventDefault();
+    const user_name = document.getElementById("user_name").value;
+    const user_email = document.getElementById("user_email").value;
+    const user_pass = document.getElementById("user_pass").value;
+    const user_school = document.getElementById("user_school").value;
+    const user_faculty = document.getElementById("user_faculty").value;
+    const user_department = document.getElementById("user_department").value;
+    const user_age = document.getElementById("user_age").value;
+    const user_date = document.getElementById("user_date").value;
+
+    // const newlogin_formdata = ()
+})
