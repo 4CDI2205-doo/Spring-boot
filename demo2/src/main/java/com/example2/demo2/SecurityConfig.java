b0651c637bba9ebec028fb73ca0d8e301199f4c3
4CDI2205-doo <btja9985@yahoo.co.jp>
2026-09-21 19:29:42 +0900

    login,session処理の追加

---
+++ demo2/src/main/java/com/example2/demo2/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.example2.demo2;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        // オリジン設定
+        configuration.setAllowedOrigins(
+                List.of("http://127.0.0.1:5500")
+        );
+        // セッション管理
+        configuration.setAllowCredentials(true);
+        // メソッド管理
+        configuration.setAllowedMethods(
+                List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        );
+        // ヘッダー管理
+        configuration.setAllowedHeaders(
+                List.of("*")
+        );
+        CorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // URL管理
+        ((UrlBasedCorsConfigurationSource) source).registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .cors(cors -> {})
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/user/newlogin","/user/login","/user/session-check").permitAll()
+                .anyRequest().authenticated()
+            );
+
+        return http.build();
+    }
+}
--- demo2/src/main/java/com/example2/demo2/UserRepository.java
+++ demo2/src/main/java/com/example2/demo2/UserRepository.java
@@ -1,6 +1,8 @@
 package com.example2.demo2;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
